60d53084189d109ddcf58684482a46fb3fd4a5e0
huatuco <chuatucorojas25@gmail.com>
2026-04-05 15:58:10 -0500

    implementación pwa v3

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,28 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
-        src: '/icon.png',
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
+      },
+       {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       },
     ],
   }
